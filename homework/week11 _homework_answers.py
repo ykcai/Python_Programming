e83e57fc4e145f432ced6e4ae402163cd51562a7
Michael Cai <ykcai@uwaterloo.ca>
2019-11-24 18:34:35 -0500

    added week 11 homework

---
+++ homework/week11 _homework_answers.py
@@ -0,0 +1,45 @@
+# Python Programming Week 11 Homework
+
+# 1. Create a function that takes a number as an argument and returns True or False depending on whether the number is symmetrical or not. A number is symmetrical when it is the same as its reverse.
+# Examples
+# is_symmetrical(7227) ➞ True
+# is_symmetrical(12567) ➞ False
+# is_symmetrical(44444444) ➞ True
+# is_symmetrical(9939) ➞ False
+# is_symmetrical(1112111) ➞ True
+
+
+def is_symmetrical(number):
+    return str(number) == str(number)[::-1]
+
+
+print(is_symmetrical(7227))
+print(is_symmetrical(12567))
+print(is_symmetrical(44444444))
+print(is_symmetrical(9939))
+print(is_symmetrical(1112111))
+
+# 2. Create a function that takes a string and returns a new string with all vowels removed.
+
+# Examples
+# remove_vowels("I have never seen a thin person drinking Diet Coke.")
+# ➞ " hv nvr sn  thn prsn drnkng Dt Ck."
+# remove_vowels("We're gonna build a wall!")
+# ➞ "W'r gnn bld  wll!"
+# remove_vowels("Happy Thanksgiving to all--even the haters and losers!")
+# ➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
+# Notes: "y" is not considered a vowel. Expect a valid string for all test cases.
+
+
+def remove_vowels(sentence):
+    vowels = 'aeiouAEIOU'
+    res = ''
+    for c in sentence:
+        if c not in vowels:
+            res += c
+    return res
+
+
+print(remove_vowels("I have never seen a thin person drinking Diet Coke."))
+print(remove_vowels("We're gonna build a wall!"))
+print(remove_vowels("Happy Thanksgiving to all--even the haters and losers!"))
