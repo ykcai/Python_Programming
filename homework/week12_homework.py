b7af59662bae66f43a12107c22e351aed023fae9
Michael Cai <ykcai@uwaterloo.ca>
2019-12-01 11:08:31 -0500

    added week 12 homework

---
+++ homework/week12_homework.py
@@ -0,0 +1,29 @@
+# Python Programming Week 12 Homework
+# 1. Difference of Max and Min Numbers in List
+# Create a function that takes a list and returns the difference between the smallest and biggest numbers.
+# Examples
+
+# difference_max_min([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+# Smallest number is -50, biggest is 32.
+
+# difference_max_min([44, 32, 86, 19]) ➞ 67
+# Smallest number is 19, biggest is 86.
+
+
+def difference_max_min(num_list):
+    pass
+
+# 2. Check if a List Contains a Given Number
+# Write a function to check if a list contains a particular number.
+# Examples
+
+# check([1, 2, 3, 4, 5], 3) ➞ True
+# check([1, 1, 2, 1, 1], 3) ➞ False
+# check([5, 5, 5, 6], 5) ➞ True
+# check([], 5) ➞ False
+
+# Notes: Don't forget to return the result.
+
+
+def check(num_list, target):
+    pass
