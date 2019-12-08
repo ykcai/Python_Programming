0e96f5c1b4aa0869527ee692ec795afac3cf159d
Michael Cai <ykcai@uwaterloo.ca>
2019-12-08 17:37:56 -0500

    added week 13 homework

---
+++ homework/week13_homework_answers.py
@@ -0,0 +1,31 @@
+# Python Programming Week 13 Homework Answers
+# 1. Convert Minutes into Seconds
+
+# Write a function that takes an integer minutes and converts it to seconds.
+# Examples
+# convert(5) ➞ 300
+# convert(3) ➞ 180
+# convert(2) ➞ 120
+
+# Notes: Don't forget to return the result. If you get stuck on a challenge, find help in the Resources tab. If you're really stuck, unlock solutions in the Solutions tab.
+
+
+def convert(minutes):
+    return minutes * 60
+
+# 2. Maximum Edge of a Triangle
+
+# Create a function that finds the maximum range of a triangle's third edge.
+# Examples
+
+# nextEdge(8, 10) ➞ 17
+# nextEdge(5, 7) ➞ 11
+# nextEdge(9, 2) ➞ 10
+# Notes
+#     (side1 + side2) - 1 = maximum range of third edge.
+#     All given triangles have side lengths that are positive integers.
+#     Don't foget to return the result.
+
+
+def nextEdge(side1, side2):
+    return side1 + side2 - 1
